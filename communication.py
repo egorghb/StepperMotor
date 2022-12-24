ab1b998ddd2794aa9cd89c1d5207e0c01a5f148a
Alina Sakharova <76452542+sakharlina@users.noreply.github.com>
2022-12-25 02:24:22 +0500

    Update communication.py
    Добавили кнопки

--- communication.py
+++ communication.py
@@ -8,15 +8,38 @@ time.sleep(2)
 
 params = [0, 0, 0] # скорость, угол, направление
 
-def link_0_up():
-    if 0 <= links[0] < 180:
-        links[0] += 1
-    label1['text'] = 'Скорость = %s' % links[0]
+def Speed_up():
+    if 0 <= params[0] < 1000:
+        params[0] += 1
+    labelSpeed['text'] = 'Скорость = %s' % params[0]
 
-def link_0_dn():
-    if 0 < links[0] <= 180:
-        links[0] -= 1
-    label1['text'] = 'Скорость = %s' % links[0]
+def Speed_dn():
+    if 0 < params[0] <= 1000:
+        params[0] -= 1
+    labelSpeed['text'] = 'Скорость = %s' % params[0]
+
+
+def Angle_up():
+    if 0 <= params[1] < 180:
+        params[1] += 1
+    labelAngle['text'] = 'Угол = %s' % params[1]
+
+def Angle_dn():
+    if 0 < params[1] <= 180:
+        params[1] -= 1
+    labelAngle['text'] = 'Угол = %s' % params[1]
+
+
+def Dir_up():
+    if params[2] == 0:
+        params[2] += 1
+    labelDir['text'] = 'Направление = %s' % params[2]
+
+
+def Dir_dn():
+    if params[2] == 1:
+        params[2] -= 1
+    labelDir['text'] = 'Направление = %s' % params[2]
 
 
 def arduino_send():
@@ -38,19 +61,39 @@ def arduino_read():
 
 
 root = Tk()
-print(root.children)
 
 frame1 = Frame(root)
+frame2 = Frame(root)
+frame3 = Frame(root)
 
-label1 = Label(frame1, text='Скорость = %s' % links[0])
-label1.pack(side='left')
+labelSpeed = Label(frame1, text='Скорость = %s' % params[0])
+labelSpeed.pack(side='left')
 
-button0_up = Button(frame1, text='+', command=link_0_dn)
-button0_up.pack(side='right')
-button0_dn = Button(frame1, text='-', command=link_0_up)
-button0_dn.pack(side='right')
+buttonSpeed_up = Button(frame1, text='+', command=Speed_up)
+buttonSpeed_up.pack(side='right')
+buttonSpeed_dn = Button(frame1, text='-', command=Speed_dn)
+buttonSpeed_dn.pack(side='right')
+
+labelAngle = Label(frame2, text='Угол = %s' % params[1])
+labelAngle.pack(side='left')
+
+buttonAngle_up = Button(frame2, text='+', command=Angle_up)
+buttonAngle_up.pack(side='right')
+buttonAngle_dn = Button(frame2, text='-', command=Angle_dn)
+buttonAngle_dn.pack(side='right')
+
+labelDir = Label(frame3, text='Направление = %s' % params[2])
+labelDir.pack(side='left')
+
+buttonDir_up = Button(frame3, text='+', command=Dir_up)
+buttonDir_up.pack(side='right')
+buttonDir_dn = Button(frame3, text='-', command=Dir_dn)
+buttonDir_dn.pack(side='right')
+
 
 frame1.pack()
+frame2.pack()
+frame3.pack()
 root.after(500, arduino_send)
 root.after(500, arduino_read)
 root.mainloop()
